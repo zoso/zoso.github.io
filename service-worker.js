0bbf68300638effdd9d09ab4eb79a2ea06966ba0
Lars Lund <Lars@Larss-MacBook-Air.local>
2016-07-28 19:16:03 +0200

    push support

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,11 @@
+
+this.onpush = function(event) {
+  console.log(event.data);
+  // From here we can write the data to IndexedDB, send it to any open
+  // windows, display a notification, etc.
+}
+
+
 self.addEventListener('install', function(e) {
 	console.log("install "+e);
 });
