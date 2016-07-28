564f0b244b7729c236ec67a947b31433d7ec5028
Lars Lund <Lars@Larss-MacBook-Air.local>
2016-07-28 20:10:11 +0200

    added cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,6 @@
 
+importScripts('./js/sw-polyfill.js');
+
 this.onpush = function(event) {
   console.log(event.data);
   // From here we can write the data to IndexedDB, send it to any open
@@ -8,6 +10,17 @@ this.onpush = function(event) {
 
 self.addEventListener('install', function(e) {
 	console.log("install "+e);
+	self.skipWaiting();
+	e.waitUntil(
+		caches.open('test-app').then(function(cache) {
+			return cache.addAll([
+				'/',
+				'/index.html',
+				'/about.html',
+				'/style.css'
+			])
+		})
+	)
 });
 
 self.addEventListener('activate', function(e) {
@@ -16,4 +29,9 @@ self.addEventListener('activate', function(e) {
 
 self.addEventListener('fetch', function(e) {
 	console.log("fetch "+e.request.url);
+	e.respondWith(
+		caches.match(e.request).then(function(response) {
+			return response || fetch(e.request);
+		});
+	);
 });
